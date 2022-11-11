7a9383c4f98a20a2abc2eff1aa255848e54e0ebf
AIMAN *͘⁴̅⁰͍⁴̵〆* <Lynxaiman@yahoo.com>
2022-11-12 06:48:06 +0800

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -3,9 +3,9 @@ async function handler(m) {
                 let vcard = 'BEGIN:VCARD\n' // metadata of the contact card
                     + 'VERSION:3.0\n'
                     + 'FN:ozan\n' // full name
-                    + 'TEL;type=CELL;type=VOICE;waid=6281227144597:+62 812-2714-4597\n' // WhatsApp ID + phone number
+                    + 'TEL;type=CELL;type=VOICE;waid=60102810046:+60 10-281 0046\n' // WhatsApp ID + phone number
                     + 'END:VCARD'
-                conn.sendMessage(m.chat, { contacts: { displayName: 'ozan', contacts: [{ vcard }] } }, { quoted: m })
+                conn.sendMessage(m.chat, { contacts: { displayName: 'Aiman', contacts: [{ vcard }] } }, { quoted: m })
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
